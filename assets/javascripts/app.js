da918c2d5fd401fa4196d8c060d8ca71f5b8031e
norman santiago <nsantiago2719@gmail.com>
2023-01-05 21:35:37 +0800

    added keyboard control on swiper

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -2,5 +2,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var swiper = new Swiper(".details-swiper", {
     effect: "cards",
     grabCursor: true,
+    keyboard: {
+      enabled: true,
+    }
   });
 });     
